757d69053931f07b5a2c336a9049ac44bf5af907
Corey Wilson <corey@commercesignals.com>
2017-03-08 10:25:11 -0500

    updated demo script

--- demo.php
+++ demo.php
@@ -3,11 +3,13 @@ const CERT_FILE_NAME = '';
 const API_KEY = '';
 const API_BASE = '';
 
-define('__ROOT__', dirname(__FILE__));
-require_once(__ROOT__ . '/Comsig/Rest/Client.php');
+require_once(__DIR__ . '/Comsig/Rest/Client.php');
 
 use Comsig\Rest\Client;
 
-$cert = file_get_contents(__ROOT__ . '/' . CERT_FILE_NAME);
+$cert = file_get_contents(__DIR__ . '/' . CERT_FILE_NAME);
 
 $client = new Client(API_KEY, $cert, API_BASE);
+
+print_r($client->getSignals());
+
