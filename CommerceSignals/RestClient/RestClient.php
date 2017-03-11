2af83223b316cb2688f1e37c581c900e3d18df41
Corey Wilson <phpuser@gmail.com>
2017-03-10 22:38:14 -0500

    display what URL is being fetched before CURL call

--- CommerceSignals/RestClient/RestClient.php
+++ CommerceSignals/RestClient/RestClient.php
@@ -131,7 +131,7 @@ class RestClient implements \Iterator, \ArrayAccess {
     }
 
     public function execute($url, $method='GET', $parameters=[], $headers=[]){
-        print 'Getting: ' . $url;
+        print "Getting: $url \n";
 
         $client = clone $this;
         $client->url = $url;
