168e1584a28d5d1dfc9489734b58b83d53ae0b4d
Corey Wilson <corey@commercesignals.com>
2017-03-16 16:23:37 -0400

    created an APIException

---
+++ src/CommerceSignals/Exceptions/APIException.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace CommerceSignals\Exceptions;
+
+class APIException extends \Exception {
+    protected $statusCode;
+    protected $errors = [];
+    /**
+     * Store the HTTP status code in a protected attribute
+     */
+    public function __construct($errorType, $errors, $statusCode) {
+      $this->statusCode = $statusCode;
+      $this->errors = $errors;
+
+      parent::__construct($errorType, $statusCode);
+    }
+
+    /**
+     * Helper method for getting the HTTP status code
+     */
+    public function getStatusCode() {
+      return $this->statusCode;
+    }
+
+    /**
+     * Helper method for getting the errors array
+     */
+    public function getErrors() {
+      return $this->errors;
+    }
+}
